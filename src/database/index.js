75330ac0b162b416eabbe52727f7d0430a57ef93
Bruno Lopes <eusoubruno7@gmail.com>
2021-04-29 16:29:26 -0300

    create techs, techs routes, many to many

--- src/database/index.js
+++ src/database/index.js
@@ -2,14 +2,18 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 const User = require('../models/User');
 const Address = require('../models/Address');
+const Tech = require('../models/Tech');
 
 
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
 Address.init(connection);
+Tech.init(connection);
+
 
 Address.associate(connection.models);
 User.associate(connection.models);
+Tech.associate(connection.models);
 
 module.exports = connection;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
 const AddressController = require('./controllers/AddressController');
+const TechController = require('./controllers/TechController');
 
 const routes = express.Router();
 
@@ -11,5 +12,9 @@ routes.get('/users', UserController.index);
 //address routes
 routes.post('/users/:user_id/addresses', AddressController.store);
 routes.get('/users/:user_id/addresses', AddressController.index);
+//Techs routes
+routes.get('/users/:user_id/techs', TechController.index);
+routes.post('/users/:user_id/techs', TechController.store);
+routes.delete('/users/:user_id/techs', TechController.delete);
 
 module.exports = routes;

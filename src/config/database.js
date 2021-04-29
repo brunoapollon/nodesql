a21f6b56000f26a27097494be7508cd9d2cac590
Bruno Lopes <eusoubruno7@gmail.com>
2021-04-29 11:32:31 -0300

    Create users

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'sqlnode',
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+};
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+   return res.json({hello: "world"});
+});
+
+module.exports = routes;
